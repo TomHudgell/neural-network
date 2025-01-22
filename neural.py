5a25f89a66d36936aebbaa8127c44c20f45ec105
TomHudgell <thomas.hudgell@gmail.com>
2025-01-22 18:11:20 +0000

    Add loss function

--- neural.py
+++ neural.py
@@ -52,3 +52,13 @@ class NeuralNetwork:
 network = NeuralNetwork()
 x = np.array([2, 3])
 print(network.feedforward(x))
+
+# mean squared error loss function
+def mse_loss(y_true, y_pred):
+    # y_true and y_pred are numpy arrays of the same length
+    return ((y_true - y_pred) ** 2).mean()
+
+y_true = np.array([1, 0, 0, 1])
+y_pred = np.array([0, 0, 0, 0])
+
+print(mse_loss(y_true, y_pred)) #0.5
